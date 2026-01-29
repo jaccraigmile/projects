3fcc111412f9963d7375fda2244af3d3a516b305
jaccraigmile <jaccraigmile@gmail.com>
2026-01-29 14:53:57 -0500

    Create setupcomplete.cmd

---
+++ setupcomplete.cmd
@@ -0,0 +1,28 @@
+@echo off
+title SetupComplete - Windows Update Control
+
+REM ===============================
+REM Disable automatic Windows Update
+REM ===============================
+
+REM Policy: Disable automatic updates
+reg add HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU ^
+ /v NoAutoUpdate /t REG_DWORD /d 1 /f
+
+REM Prevent auto-restart for updates
+reg add HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU ^
+ /v NoAutoRebootWithLoggedOnUsers /t REG_DWORD /d 1 /f
+
+REM Set updates to "Notify only" if user enables them later
+reg add HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU ^
+ /v AUOptions /t REG_DWORD /d 2 /f
+
+REM Disable Update Orchestrator (Windows 10/11)
+sc config UsoSvc start= disabled
+sc stop UsoSvc
+
+REM Disable Windows Update service
+sc config wuauserv start= disabled
+sc stop wuauserv
+
+exit /b 0
